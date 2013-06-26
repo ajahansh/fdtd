8ca762e12d00dcadf8bab5e133266e3ec3d81a72
Amir Jahanshahi <Amir.Jahanshahi@chmail.ir>
2013-06-26 13:14:41 +0200

    Final-Modified Kappa

--- Thin_Wall/Fresnel_Diffraction/frequency_response.m
+++ Thin_Wall/Fresnel_Diffraction/frequency_response.m
@@ -2,42 +2,101 @@ clear all
 clc
 close all
 global nu;
-nu=0:2700;
-c = 340;
-k = 2*pi*nu/c;
-%% Source Reflection Recorder Normal
-a = sqrt(13)/2;
-b = a;
-theta_r = 2*pi - atan(2/3);
-theta_s = atan(2/3);
+nu=linspace(1,2700,1000);
+c=340;
+k=2*pi*nu/c;
+%% Source Normal Recorder Normal - Recorder 1
+a=sqrt(13)/2;
+b=a;
+theta_r=2*pi-atan(2/3);
+theta_s=atan(2/3);
 
-data1 = Fresnel_Diffraction(a, b, theta_r, theta_s);
+data1=Fresnel_Diffraction(a, b, theta_r, theta_s);
+%% Source Reflection Recorder Normal - Recorder 1
+a=sqrt(29)/2;
+b=sqrt(13)/2;
+theta_r=2*pi-atan(2/3);
+theta_s=atan(2/5);
 
-%% Source Reflection Recorder Normal
-a = sqrt(29)/2;
-b = sqrt(13)/2;
-theta_r = 2*pi - atan(2/3);
-theta_s = atan(2/5);
+data2=Fresnel_Diffraction(a, b, theta_r, theta_s);
+%% Source Reflection Recorder Reflection - Recorder 1
+a=sqrt(29)/2;
+b=a;
+theta_r=2*pi-atan(2/5);
+theta_s=atan(2/5);
 
-data2 = Fresnel_Diffraction(a, b, theta_r, theta_s);
+data3=Fresnel_Diffraction(a,b,theta_r,theta_s);
+%% Source normal Recorder Reflection - Recorder 1
+a=sqrt(13)/2;
+b=sqrt(29)/2;
+theta_r=2*pi-atan(2/5);
+theta_s=atan(2/3);
 
-%% Source Reflection Recorder Reflection
-a = sqrt(29)/2;
-b = a;
-theta_r = 2*pi - atan(2/5);
-theta_s = atan(2/5);
+data4=Fresnel_Diffraction(a, b, theta_r, theta_s);
+semilogy(k,abs(data1-data2+data3-data4),'r')
+hold on
+%% Source Normal Recorder Normal - Recorder 2
+a=sqrt(13)/2;
+b=sqrt(25)/2;
+theta_r=2*pi-atan(4/3);
+theta_s=atan(2/3);
 
-data3 = Fresnel_Diffraction(a, b, theta_r, theta_s);
+data1=Fresnel_Diffraction(a, b, theta_r, theta_s);
+%% Source Reflection Recorder Normal - Recorder 2
+a=sqrt(29)/2;
+b=sqrt(25)/2;
+theta_r=2*pi-atan(4/3);
+theta_s=atan(2/5);
 
-%% Source normal Recorder Reflection
-a = sqrt(13)/2;
-b = sqrt(29)/2;
-theta_r = 2*pi - atan(2/5);
-theta_s = atan(2/3);
+data2=Fresnel_Diffraction(a, b, theta_r, theta_s);
+%% Source Reflection Recorder Reflection - Recorder 2
+a=sqrt(29)/2;
+b=sqrt(41)/2;
+theta_r=2*pi-atan(4/5);
+theta_s=atan(2/5);
 
-data4 = Fresnel_Diffraction(a, b, theta_r, theta_s);
-semilogy(k,abs(data1-data2+data3-data4))
+data3=Fresnel_Diffraction(a,b,theta_r,theta_s);
+%% Source normal Recorder Reflection - Recorder 2
+a=sqrt(13)/2;
+b=sqrt(41)/2;
+theta_r=2*pi-atan(4/5);
+theta_s=atan(2/3);
+
+data4=Fresnel_Diffraction(a, b, theta_r, theta_s);
+semilogy(k,abs(data1-data2+data3-data4),'g')
+%% Source Normal Recorder Normal - Recorder 3
+a=sqrt(13)/2;
+b=sqrt(45)/2;
+theta_r=2*pi-atan(2);
+theta_s=atan(2/3);
+
+data1=Fresnel_Diffraction(a, b, theta_r, theta_s);
+%% Source Reflection Recorder Normal - Recorder 3
+a=sqrt(29)/2;
+b=sqrt(45)/2;
+theta_r=2*pi-atan(2);
+theta_s=atan(2/5);
+
+data2=Fresnel_Diffraction(a, b, theta_r, theta_s);
+%% Source Reflection Recorder Reflection - Recorder 3
+a=sqrt(29)/2;
+b=sqrt(61)/2;
+theta_r=2*pi-atan(6/5);
+theta_s=atan(2/5);
+
+data3=Fresnel_Diffraction(a,b,theta_r,theta_s);
+%% Source normal Recorder Reflection - Recorder 3
+a=sqrt(13)/2;
+b=sqrt(61)/2;
+theta_r=2*pi-atan(6/5);
+theta_s=atan(2/3);
+
+data4=Fresnel_Diffraction(a, b, theta_r, theta_s);
+semilogy(k,abs(data1-data2+data3-data4),'b')
+
 xlabel('Kd')
 ylabel('Absolute Fresnel')
-matlab2tikz('fresnel_diffraction_r1.tikz', 'height', '\figureheight', 'width', ...
+legend('Recorder 1', 'Recorder 2', 'Recorder 3')
+saveas(gcf,'fresnel_diffraction','pdf')
+matlab2tikz('fresnel_diffraction.tikz', 'height', '\figureheight', 'width', ...
             '\figurewidth','showInfo',false);
